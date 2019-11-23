f0c63553993bceb8c18956add05eacd64051ca51
Donna <dmcruz@gmail.com>
2019-11-23 21:18:59 +0800

    update packages via npm audit; fix error in mac by using kestrel server

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -20,6 +21,14 @@ namespace VueCore
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseKestrel((context, serverOptions) =>
+                {
+                    serverOptions.Configure(context.Configuration.GetSection("Kestrel"))
+                        .Endpoint("HTTPS", listenOptions =>
+                        {
+                            listenOptions.HttpsOptions.SslProtocols = SslProtocols.Tls12;
+                        });
+                })
                 .Build();
     }
 }
